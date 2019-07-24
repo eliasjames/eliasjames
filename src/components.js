c49b202ffbd90d0f45c984076997d0b9970dcc91
Elias Carlston <elias@eliascarlston.com>
2019-07-23 21:57:02 -0400

    Copied from overvue

---
+++ src/components.js
@@ -0,0 +1,12 @@
+module.exports = {
+  A: Vue.component('defaultContent', {
+    template: `<p>
+            A JavaScript library for quickly building Single Page Apps
+          </p>`
+  }),
+  B: Vue.component('alternateContent', {
+    template: `<p>
+            Here goes something else
+          </p>`
+  })
+};
